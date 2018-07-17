34cbd65c9c04d592a6e5cb71e60f0daef5cf2e6c
Sheila HK <38942403+tigershk@users.noreply.github.com>
2018-07-17 17:27:10 +0100

    calendar added, events can be added to own calendar but no notification for dmitri, map etc to be added

--- src/containers/ContactContainer.js
+++ src/containers/ContactContainer.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux";
 import Contact from "../components/Contact"
 import { fetchCalendar } from "../actions";
-//Sheila was here
+
 
 
 const mapDispatchToProps = dispatch => {
